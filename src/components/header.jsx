d43b3f21113c38287c16507881615f662d8ba9d9
karorinn <120996298+karorinn@users.noreply.github.com>
2024-03-14 13:18:37 +0300

    Update header.jsx

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -3,8 +3,7 @@ function Header(){
         <nav className="page-header pink garken-3">
             <div className="container">
                 <div className="naw-wrapper">
-                    <a href="!#" className="logo"><h4>ShoppingCenter</h4>
-                    </a>
+                    <a href="!#" className="logo">ShoppingCenter</a>
                     <ul id="nav-mobile" className="right hide-on-med-and-down">
                         <li><a href="https://github.com/karorinn/shopping_center">Repository</a></li>
                     </ul>
